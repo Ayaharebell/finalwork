9afd25ea54067d1facdcad0df61572b90cfbc04a
zyy <Shiyeyoufang@163.com>
2021-11-30 22:00:43 +0800

    test

---
+++ final/Form_solve.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class Form_solve : WeifenLuo.WinFormsUI.Docking.DockContent
+    {
+        public Form_solve()
+        {
+            InitializeComponent();
+        }
+    }
+}
